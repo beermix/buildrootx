3a5ab03df94f7e28ca24d0d6190b96047334530e
AV <27rurk@gmail.com>
2018-07-04 01:41:54 +0300

    autocommit on change

--- package/at-spi2-core/at-spi2-core.mk
+++ package/at-spi2-core/at-spi2-core.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-AT_SPI2_CORE_VERSION_MAJOR = 2.29.1
+AT_SPI2_CORE_VERSION_MAJOR = 2.28
 AT_SPI2_CORE_VERSION = $(AT_SPI2_CORE_VERSION_MAJOR).0
 AT_SPI2_CORE_SOURCE = at-spi2-core-$(AT_SPI2_CORE_VERSION).tar.xz
 AT_SPI2_CORE_SITE = http://ftp.gnome.org/pub/gnome/sources/at-spi2-core/$(AT_SPI2_CORE_VERSION_MAJOR)
@@ -13,12 +13,5 @@ AT_SPI2_CORE_LICENSE_FILES = COPYING
 AT_SPI2_CORE_INSTALL_STAGING = YES
 AT_SPI2_CORE_DEPENDENCIES = host-pkgconf dbus libglib2 xlib_libXtst
 AT_SPI2_CORE_CONF_OPTS = -Ddbus_daemon=/usr/bin/dbus-daemon
-
-ATK_CONF_OPTS += \
-	-Denable_docs=false \
-	-Denable-introspection=no
-
-ATK_CONF_ENV = $(HOST_UTF8_LOCALE_ENV)
-ATK_NINJA_ENV = $(HOST_UTF8_LOCALE_ENV)
 
 $(eval $(meson-package))
