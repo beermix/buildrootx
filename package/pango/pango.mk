465775eb51d88a7ed466a089879e76a2d42e9a39
AV <27rurk@gmail.com>
2018-07-05 22:48:20 +0300

    autocommit on change

--- package/pango/pango.mk
+++ package/pango/pango.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-PANGO_VERSION_MAJOR = 1.42
-PANGO_VERSION = $(PANGO_VERSION_MAJOR).1
+PANGO_VERSION_MAJOR = 1.40
+PANGO_VERSION = $(PANGO_VERSION_MAJOR).14
 PANGO_SOURCE = pango-$(PANGO_VERSION).tar.xz
 PANGO_SITE = http://ftp.gnome.org/pub/GNOME/sources/pango/$(PANGO_VERSION_MAJOR)
 PANGO_AUTORECONF = YES
@@ -13,8 +13,8 @@ PANGO_INSTALL_STAGING = YES
 PANGO_LICENSE = LGPL-2.0+
 PANGO_LICENSE_FILES = COPYING
 
-PANGO_CONF_OPTS = -Denable_docs=false -Dgir=false
-HOST_PANGO_CONF_OPTS = -Denable_docs=false -Dgir=false
+PANGO_CONF_OPTS = --enable-explicit-deps=no
+HOST_PANGO_CONF_OPTS = --enable-explicit-deps=no
 
 PANGO_DEPENDENCIES = \
 	$(TARGET_NLS_DEPENDENCIES) \
@@ -23,14 +23,26 @@ PANGO_DEPENDENCIES = \
 	cairo \
 	harfbuzz \
 	fontconfig \
-	freetype xlib_libXft xlib_libXrender libfribidi xlib_libX11
+	freetype
 HOST_PANGO_DEPENDENCIES = \
 	host-pkgconf \
 	host-libglib2 \
 	host-cairo \
 	host-harfbuzz \
 	host-fontconfig \
-	host-freetype host-libfribidi
+	host-freetype
 
-$(eval $(meson-package))
-$(eval $(host-meson-package))
+ifeq ($(BR2_PACKAGE_XORG7),y)
+PANGO_CONF_OPTS += \
+	--x-includes=$(STAGING_DIR)/usr/include/X11 \
+	--x-libraries=$(STAGING_DIR)/usr/lib
+PANGO_DEPENDENCIES += xlib_libX11
+endif
+
+ifeq ($(BR2_PACKAGE_XLIB_LIBXFT)$(BR2_PACKAGE_XLIB_LIBXRENDER),yy)
+PANGO_DEPENDENCIES += xlib_libXft xlib_libXrender
+PANGO_CONF_OPTS += --with-xft
+endif
+
+$(eval $(autotools-package))
+$(eval $(host-autotools-package))
