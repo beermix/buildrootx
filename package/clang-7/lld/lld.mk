618ae172688e9a8122665c2190bb5c97e14e83f0
AV <27rurk@gmail.com>
2018-08-16 21:03:07 +0300

    autocommit on change

--- package/clang-7/lld/lld.mk
+++ package/clang-7/lld/lld.mk
@@ -1,8 +1,8 @@
-###############################################################################
+################################################################################
 #
 # lld
 #
-###############################################################################
+################################################################################
 
 LLD_VERSION = 47f841f
 LLD_SITE = $(call github,llvm-mirror,lld,$(LLD_VERSION))
@@ -10,17 +10,6 @@ LLD_SITE = $(call github,llvm-mirror,lld,$(LLD_VERSION))
 LLD_LICENSE = NCSA
 LLD_LICENSE_FILES = LICENSE.TXT
 LLD_SUPPORTS_IN_SOURCE_BUILD = NO
-HOST_LLD_DEPENDENCIES = host-python host-ninja
+HOST_LLD_DEPENDENCIES = host-llvm
 
-HOST_LLD_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
-#HOST_LLD_CONF_OPTS += -DPYTHON_EXECUTABLE=$(HOST_DIR)/bin/python2
-#HOST_LLD_CONF_OPTS += -DBUILD_SHARED_LIBS=ON
-#HOST_LLD_CONF_OPTS += -DLLVM_LINK_LLVM_DYLIB=ON
-#HOST_LLD_CONF_OPTS += -DLLVM_INCLUDE_TESTS=OFF
-#HOST_LLD_CONF_OPTS += -DLLVM_BUILD_TESTS=OFF
-#HOST_LLD_CONF_OPTS += -DLLVM_BUILD_DOCS=OFF
-
-LLD_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
-
-$(eval $(cmake-package))
 $(eval $(host-cmake-package))
