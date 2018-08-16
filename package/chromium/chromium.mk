e4e09a0eeadf97aac061b23017fc8d19a4fb87ff
AV <27rurk@gmail.com>
2018-08-16 18:42:45 +0300

    autocommit on change

--- package/chromium/chromium.mk
+++ package/chromium/chromium.mk
@@ -1,6 +1,6 @@
 ################################################################################
 #
-# Chromium v8_snapshot_toolchain=\"$(CHROMIUM_TOOLCHAIN_CONFIG_PATH):v8_snapshot\" \
+# Chromium
 #
 ################################################################################
 
@@ -9,54 +9,71 @@ CHROMIUM_SITE = https://commondatastorage.googleapis.com/chromium-browser-offici
 CHROMIUM_SOURCE = chromium-$(CHROMIUM_VERSION).tar.xz
 CHROMIUM_LICENSE = BSD-Style
 CHROMIUM_LICENSE_FILES = LICENSE
-CHROMIUM_DEPENDENCIES = systemd alsa-lib cairo dbus freetype harfbuzz \
-			host-ninja host-python \
-			jpeg-turbo libdrm libglib2 libva libkrb5 libnss libpng pango \
+CHROMIUM_DEPENDENCIES = alsa-lib cairo fontconfig freetype \
+			harfbuzz host-clang host-ninja host-nodejs host-python \
+			icu jpeg libdrm libglib2 libkrb5 libnss libpng \
+			libxml2 libxslt pango \
 			xlib_libXcomposite xlib_libXScrnSaver xlib_libXcursor \
-			xlib_libXrandr zlib host-clang host-lld libgtk2 xlib_libXi xlib_libXtst
+			xlib_libXrandr zlib libva
 
 CHROMIUM_TOOLCHAIN_CONFIG_PATH = $(shell pwd)/package/chromium/toolchain
 
 CHROMIUM_OPTS = \
 	host_toolchain=\"$(CHROMIUM_TOOLCHAIN_CONFIG_PATH):host\" \
 	custom_toolchain=\"$(CHROMIUM_TOOLCHAIN_CONFIG_PATH):target\" \
-	use_lld=false \
-	is_clang=false \
+	v8_snapshot_toolchain=\"$(CHROMIUM_TOOLCHAIN_CONFIG_PATH):v8_snapshot\" \
+	is_clang=true \
 	clang_use_chrome_plugins=false \
 	treat_warnings_as_errors=false \
 	use_gnome_keyring=false \
 	linux_use_bundled_binutils=false \
-	use_sysroot=true \
+	use_sysroot=false \
 	target_sysroot=\"$(STAGING_DIR)\" \
 	target_cpu=\"$(BR2_PACKAGE_CHROMIUM_TARGET_ARCH)\" \
 	google_api_key=\"AIzaSyAQ6L9vt9cnN4nM0weaa6Y38K4eyPvtKgI\" \
 	google_default_client_id=\"740889307901-4bkm4e0udppnp1lradko85qsbnmkfq3b.apps.googleusercontent.com\" \
 	google_default_client_secret=\"9TJlhL661hvShQub4cWhANXa\" \
 	enable_nacl=false \
-	use_dbus=true \
+	enable_swiftshader=false \
+	enable_linux_installer=false \
+	is_official_build=true \
 	use_system_zlib=true \
-	use_system_libdrm=true \
+	use_system_libjpeg=true \
+	use_system_libpng=true \
 	use_system_harfbuzz=true \
 	use_system_freetype=true \
-	use_system_libjpeg=true \
-	use_system_libpng=false \
-	linux_link_libudev=true \
-	symbol_level=0 \
-	fatal_linker_warnings=false \
-	fieldtrial_testing_like_official_build=true \
-	remove_webcore_debug_symbols=true \
-	enable_widevine=true \
-	enable_hangout_services_extension=true \
-	enable_vulkan=false \
-	use_cups=false \
-	enable_vr=false \
 	use_custom_libcxx=false \
-	enable_swiftshader=false \
-	is_official_build=true \
 	use_vaapi=true
 
+CHROMIUM_SYSTEM_LIBS = \
+	fontconfig \
+	freetype \
+	harfbuzz-ng \
+	icu \
+	libdrm \
+	libjpeg \
+	libxml \
+	libxslt
+
+ifeq ($(BR2_i386)$(BR2_x86_64),y)
+CHROMIUM_SYSTEM_LIBS += yasm
+CHROMIUM_DEPENDENCIES += host-yasm
+endif
 # tcmalloc has portability issues
 CHROMIUM_OPTS += use_allocator=\"none\"
+ifeq ($(BR2_CCACHE),y)
+CHROMIUM_CC_WRAPPER = ccache
+endif
+
+# LLD is unsupported on i386, and fails during linking
+ifeq ($(BR2_i386)$(BR2_mips)$(BR2_mipsel)$(BR2_mips64)$(BR2_mips64el),y)
+CHROMIUM_OPTS += use_lld=false
+# Disable gold as well, to force usage of our toolchain's ld.bfd
+CHROMIUM_OPTS += use_gold=false
+else
+CHROMIUM_DEPENDENCIES += host-lld
+CHROMIUM_OPTS += use_lld=true
+endif
 
 # V8 snapshots require compiling V8 with the same word size as the target
 # architecture, which means the host needs to have that toolchain available.
@@ -68,14 +85,15 @@ else
 CHROMIUM_OPTS += is_debug=false
 endif
 
-ifeq ($(BR2_PACKAGE_CHROMIUM_PROPRIETARY_CODECS),y)
-CHROMIUM_OPTS += proprietary_codecs=true ffmpeg_branding=\"Chrome\"
+ifeq ($(BR2_PACKAGE_CUPS),y)
+CHROMIUM_DEPENDENCIES += cups
+CHROMIUM_OPTS += use_cups=true
+else
+CHROMIUM_OPTS += use_cups=false
 endif
 
-ifeq ($(BR2_PACKAGE_DBUS),y)
-CHROMIUM_OPTS += use_dbus=true
-else
-CHROMIUM_OPTS += use_dbus=false
+ifeq ($(BR2_PACKAGE_CHROMIUM_PROPRIETARY_CODECS),y)
+CHROMIUM_OPTS += proprietary_codecs=true ffmpeg_branding=\"Chrome\"
 endif
 
 ifeq ($(BR2_PACKAGE_PCIUTILS),y)
@@ -92,6 +110,13 @@ else
 CHROMIUM_OPTS += use_pulseaudio=false
 endif
 
+ifeq ($(BR2_PACKAGE_LIBGTK3_X11),y)
+CHROMIUM_DEPENDENCIES += libgtk3
+CHROMIUM_OPTS += use_gtk3=true
+else
+CHROMIUM_DEPENDENCIES += libgtk2 xlib_libXi xlib_libXtst
+CHROMIUM_OPTS += use_gtk3=false
+endif
 ifeq ($(BR2_TOOLCHAIN_EXTERNAL),y)
 CHROMIUM_TARGET_LDFLAGS += --gcc-toolchain=$(TOOLCHAIN_EXTERNAL_INSTALL_DIR)
 else
@@ -101,29 +126,57 @@ endif
 CHROMIUM_TARGET_CFLAGS += $(CHROMIUM_TARGET_LDFLAGS)
 CHROMIUM_TARGET_CXXFLAGS += $(CHROMIUM_TARGET_CFLAGS)
 
+define CHROMIUM_CONFIGURE_CMDS
+	# Allow building against system libraries in official builds
+	( cd $(@D); \
+		sed -i 's/OFFICIAL_BUILD/GOOGLE_CHROME_BUILD/' \
+			tools/generate_shim_headers/generate_shim_headers.py \
+	)
 export CCACHE_SLOPPINESS=time_macros
 
-define CHROMIUM_CONFIGURE_CMDS
+	# Use python2 by default
+	mkdir -p $(@D)/bin
+	ln -sf $(HOST_DIR)/usr/bin/python2 $(@D)/bin/python
+
+	( cd $(@D); \
+		for _lib in $(CHROMIUM_SYSTEM_LIBS); do \
+			find "third_party/$$_lib" -type f \
+			  \! -path "third_party/$$_lib/chromium/*" \
+			  \! -path "third_party/$$_lib/google/*" \
+			  \! -path "third_party/yasm/run_yasm.py" \
+			  \! -regex '.*\.\(gn\|gni\|isolate\)' \
+			  -delete; \
+		done \
+	)
 	( cd $(@D); \
 		$(TARGET_MAKE_ENV) \
-		sed -i 's/OFFICIAL_BUILD/GOOGLE_CHROME_BUILD/' tools/generate_shim_headers/generate_shim_headers.py; \
+		build/linux/unbundle/replace_gn_files.py \
+			--system-libraries $(CHROMIUM_SYSTEM_LIBS) \
+	)
+
+	( cd $(@D); \
+		$(TARGET_MAKE_ENV) \
+		AR="$(HOSTAR)" \
+		CC="$(HOSTCC)" \
+		CXX="$(HOSTCXX)" \
 		$(HOST_DIR)/bin/python2 tools/gn/bootstrap/bootstrap.py -s --no-clean; \
-		sed -i -e '/"-Wno-ignored-pragma-optimize"/d' build/config/compiler/BUILD.gn; \
 		HOST_AR="$(HOSTAR)" \
-		HOST_NM="$(HOSTNM)" \
 		HOST_CC="$(HOSTCC)" \
-		HOST_CXX="$(HOSTCXX)" \
 		HOST_CFLAGS="$(HOST_CFLAGS)" \
+		HOST_CXX="$(HOSTCXX)" \
 		HOST_CXXFLAGS="$(HOST_CXXFLAGS)" \
+		HOST_NM="$(HOSTNM)" \
 		TARGET_AR="ar" \
-		TARGET_NM="nm" \
-		TARGET_READELF="readelf" \
-		TARGET_CC="ccache clang" \
-		TARGET_CXX="ccache clang++" \
-		TARGET_CFLAGS="$(CHROMIUM_TARGET_CFLAGS) -fdiagnostics-color=always -fno-unwind-tables -fno-asynchronous-unwind-tables" \
-		TARGET_CXXFLAGS="$(CHROMIUM_TARGET_CXXFLAGS) -fdiagnostics-color=always -fno-unwind-tables -fno-asynchronous-unwind-tables" \
-		TARGET_CPPFLAGS="$(TARGET_CPPFLAGS) -DNO_UNWIND_TABLES" \
+		TARGET_CC="$(CHROMIUM_CC_WRAPPER) clang" \
+		TARGET_CFLAGS="$(CHROMIUM_TARGET_CFLAGS)" \
+		TARGET_CXX="$(CHROMIUM_CC_WRAPPER) clang++" \
+		TARGET_CXXFLAGS="$(CHROMIUM_TARGET_CXXFLAGS)" \
 		TARGET_LDFLAGS="$(CHROMIUM_TARGET_LDFLAGS)" \
+		TARGET_NM="nm" \
+		V8_AR="$(HOSTAR)" \
+		V8_CC="$(CHROMIUM_CC_WRAPPER) clang" \
+		V8_CXX="$(CHROMIUM_CC_WRAPPER) clang++" \
+		V8_NM="$(HOSTNM)" \
 		out/Release/gn gen out/Release --args="$(CHROMIUM_OPTS)" \
 			--script-executable=$(HOST_DIR)/bin/python2 \
 	)
@@ -132,12 +185,13 @@ endef
 define CHROMIUM_BUILD_CMDS
 	( cd $(@D); \
 		$(TARGET_MAKE_ENV) \
+		PATH=$(@D)/bin:$(BR_PATH) \
 		ninja -j$(PARALLEL_JOBS) -C out/Release chrome chrome_sandbox chromedriver \
 	)
 endef
 
 define CHROMIUM_INSTALL_TARGET_CMDS
-	$(INSTALL) -D $(@D)/out/Release/chrome $(TARGET_DIR)/usr/lib/chromium/chrome
+	$(INSTALL) -D $(@D)/out/Release/chrome $(TARGET_DIR)/usr/lib/chromium/chromium
 	$(INSTALL) -Dm4755 $(@D)/out/Release/chrome_sandbox \
 		$(TARGET_DIR)/usr/lib/chromium/chrome-sandbox
 	cp $(@D)/out/Release/{chrome_{100,200}_percent,resources}.pak \
