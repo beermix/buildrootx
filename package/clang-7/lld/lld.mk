088331270c7fb2a2de744c2b428dca5c77df60bb
AV <27rurk@gmail.com>
2018-09-03 16:50:52 +0300

    autocommit on change

--- package/clang-7/lld/lld.mk
+++ package/clang-7/lld/lld.mk
@@ -4,9 +4,8 @@
 #
 ################################################################################
 
-LLD_VERSION = 6.0.1
-LLD_SITE = https://llvm.org/releases/$(LLD_VERSION)
-LLD_SOURCE = lld-$(LLD_VERSION).src.tar.xz
+LLD_VERSION = f44c9f4
+LLD_SITE = $(call github,llvm-mirror,lld,$(LLD_VERSION))
 LLD_LICENSE = NCSA
 LLD_LICENSE_FILES = LICENSE.TXT
 LLD_SUPPORTS_IN_SOURCE_BUILD = NO
