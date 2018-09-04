e25432b6bcf63161dea4b2e156b31e9b8b189427
AV <27rurk@gmail.com>
2018-09-04 04:48:48 +0300

    autocommit on change

--- package/ninja/ninja.mk
+++ package/ninja/ninja.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-NINJA_VERSION = ca041d8
+NINJA_VERSION = v1.8.2
 NINJA_SITE = $(call github,ninja-build,ninja,$(NINJA_VERSION))
 NINJA_LICENSE = Apache-2.0
 NINJA_LICENSE_FILES = COPYING
 
-HOST_NINJA_DEPENDENCIES = $(if $(BR2_PACKAGE_PYTHON3),host-python3,host-python)
+HOST_NINJA_DEPENDENCIES = host-python host-python3
 
 define HOST_NINJA_BUILD_CMDS
 	(cd $(@D); $(HOST_DIR)/bin/python2 ./configure.py --bootstrap)
