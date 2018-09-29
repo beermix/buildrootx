275feaa2550bd8037a34f844b45a23002c28382f
AV <27rurk@gmail.com>
2018-09-29 16:09:01 +0300

    autocommit on change

--- package/lld/lld.mk
+++ package/lld/lld.mk
@@ -13,7 +13,7 @@ LLD_LICENSE = NCSA
 LLD_LICENSE_FILES = LICENSE.TXT
 LLD_SUPPORTS_IN_SOURCE_BUILD = NO
 HOST_LLD_DEPENDENCIES = host-llvm host-clang
-# HOST_LLD_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
+HOST_LLD_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
 
 export CCACHE_SLOPPINESS=file_macro,time_macros,include_file_mtime,include_file_ctime
 
